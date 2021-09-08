066bbe4ad14a3cba73cf82c5a51cf83f4a42cd21
Thomjo55 <thomjo@email.unc.edu>
2021-09-08 17:43:45 -0400

    changes to ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -6,7 +6,7 @@ counter: int = 0
 letter: str = input("What letter do you want to search for?")
 word: str = input("Enter a word.")
 while counter < len(word):
-    if letter == word
+    if letter == word:
         counter = counter + 1
 
 print(counter)
